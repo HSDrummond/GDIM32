3c4cbed64b11710cc3e6e7f90aa78f0f392ad90c
Hunter Drummond <85197375+HSDrummond@users.noreply.github.com>
2022-01-22 13:29:37 -0800

    Tried working on game manager and crop following

--- Game-GDIM32/Assets/Scripts/Player/PlayerMovement.cs
+++ Game-GDIM32/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    public int m_PlayerNumber = 0;
 
     public float moveSpeed = 15f;
 
     public Rigidbody2D rb;
 
+    public Transform inven_Prefab;
+
     Vector2 movement;
+
+    private List<Transform> m_Inventory;
+
+    private void Start()
+    {
+        m_Inventory = new List<Transform>();
+        m_Inventory.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -21,7 +32,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = m_Inventory.Count - 1; i > 0; i--)
+        {
+            m_Inventory[i].position = m_Inventory[i - 1].position;
+        }
         //Physics
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Follow()
+    {
+        Transform follower = Instantiate(this.inven_Prefab);
+        follower.position = m_Inventory[m_Inventory.Count - 1].position;
+
+        m_Inventory.Add(follower);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Crop"))
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Follow();
+            }
+           
+        }
     }
 }
