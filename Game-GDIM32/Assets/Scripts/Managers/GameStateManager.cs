dd08e9239f8fd188afbe7016355d89235c9eb233
dfu7 <78887201+dfu7@users.noreply.github.com>
2022-01-24 19:32:07 -0800

    Update GameStateManager.cs

--- Game-GDIM32/Assets/Scripts/Managers/GameStateManager.cs
+++ Game-GDIM32/Assets/Scripts/Managers/GameStateManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateManager : MonoBehaviour
+public class GameStateManager : GenericSingletonClass<GameStateManager>
 {
     // Start is called before the first frame update
     void Start()
