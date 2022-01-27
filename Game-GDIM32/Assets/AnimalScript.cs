7421d84a422bebc5071722ecfc0f15f6cf6ed8be
rororo707 <rororo808@gmail.com>
2022-01-26 18:11:03 -0800

    animals BASIC
    needs testing, go to test scene then hit play button at top

---
+++ Game-GDIM32/Assets/AnimalScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalScript : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject Player;
+        Player = GameObject.FindWithTag("Player");
+        target = Player.transform;
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0,-90, 0), Space.Self);
+
+        float DistanceFromPlayer = 3f;
+        if (Vector3.Distance(transform.position, target.position) > DistanceFromPlayer)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0 ,0));
+        }
+    }
+}
