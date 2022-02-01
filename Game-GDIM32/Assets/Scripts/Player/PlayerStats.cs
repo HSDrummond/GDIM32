30e1b6de74db06d990c8c6939091229475917259
dfu7 <78887201+dfu7@users.noreply.github.com>
2022-01-31 19:55:39 -0800

    Update PlayerStats.cs

--- Game-GDIM32/Assets/Scripts/Player/PlayerStats.cs
+++ Game-GDIM32/Assets/Scripts/Player/PlayerStats.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 public class PlayersStats : MonoBehaviour
 {
     static public int p1_item_count;
+    static public int p1_crop_count;
+    static public int p1_animal_count;
+
     static public int p2_item_count;
+    static public int p2_crop_count;
+    static public int p2_animal_count;
 }
