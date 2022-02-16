6a1dd628771a4409a834044a3690de54a09465e7
dfu7 <78887201+dfu7@users.noreply.github.com>
2022-02-15 20:19:08 -0800

    deleted all movement bs

--- Game-GDIM32/Assets/Scripts/Managers/PlayerManager.cs
+++ Game-GDIM32/Assets/Scripts/Managers/PlayerManager.cs
@@ -11,31 +11,12 @@ public class PlayerManager
     [HideInInspector] public GameObject m_Instance;          
     [HideInInspector] public int m_Wins;                     
 
-
-    private PlayerMovement m_Movement;       
+     
     private GameObject m_CanvasGameObject;
 
 
     public void Setup()
     {
-        m_Movement = m_Instance.GetComponent<PlayerMovement>();
-        //m_Movement.m_PlayerNumber = m_PlayerNumber;
-    }
-
-
-    public void DisableControl()
-    {
-        m_Movement.enabled = false;
-
-        m_CanvasGameObject.SetActive(false);
-    }
-
-
-    public void EnableControl()
-    {
-        m_Movement.enabled = true;
-
-        m_CanvasGameObject.SetActive(true);
     }
 
 
