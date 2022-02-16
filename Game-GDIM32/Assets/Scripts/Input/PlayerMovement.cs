7043d2ce666bf0707b47abd86da2d9dd7c630352
dfu7 <78887201+dfu7@users.noreply.github.com>
2022-02-15 20:44:55 -0800

    MOVEMENT INPUT WORKS FUCK

---
+++ Game-GDIM32/Assets/Scripts/Input/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 15f;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    private Vector2 movement = Vector2.zero;
+
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("aaa");
+        movement = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+
+
+}
