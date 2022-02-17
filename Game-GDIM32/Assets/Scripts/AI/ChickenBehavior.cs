92fc5141248d4cf27783ff617131ae6bc277c321
Hunter Drummond <85197375+HSDrummond@users.noreply.github.com>
2022-02-17 15:22:36 -0800

    AI Behavior tree Checkpoint

--- Game-GDIM32/Assets/Scripts/AI/ChickenBehavior.cs
+++ Game-GDIM32/Assets/Scripts/AI/ChickenBehavior.cs
@@ -1,14 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChickenBehavior : MonoBehaviour
 {
     BehaviorTree tree;
+    public GameObject diamond;
+    public GameObject van;
+    NavMeshAgent agent;
+
+    public enum ActionState { IDLE, WORKING };
+    ActionState state = ActionState.IDLE;
+
+    Node.Status treeStatus = Node.Status.RUNNING;
 
     private void Start()
     {
+        agent = this.GetComponent<NavMeshAgent>();
+
         tree = new BehaviorTree();
-        //node
+        Sequence steal = new Sequence("Steal Something");
+        Leaf goToDiamond = new Leaf("Go To Diamond", GoToDiamond);
+        Leaf goToVan = new Leaf("Go To Van", GoToVan);
+
+        //Add in reverse of order you want
+        steal.AddChild(goToDiamond);
+        steal.AddChild(goToVan);
+        steal.AddChild(steal);
+
+        tree.PrintTree();
     }
+
+    public Node.Status GoToDiamond()
+    {
+        return GoToLocation(diamond.transform.position);
+    }
+
+    public Node.Status GoToVan()
+    {
+        return GoToLocation(van.transform.position);
+    }
+
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTarget = Vector3.Distance(destination, this.transform.position);
+        if(state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if (Vector3.Distance(agent.pathEndPosition, destination) >= 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if(distanceToTarget < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.RUNNING;
+    }
+
+    private void Update()
+    {
+        if(treeStatus == Node.Status.RUNNING)
+        tree.Process();
+    }
+
 }
--- Game-GDIM32/Assets/Scripts/AI/Node.cs
+++ Game-GDIM32/Assets/Scripts/AI/Node.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Node
 {
-    public enum Status { SUCCESS, RUNNING, FAILURE};
+    public enum Status { SUCCESS, RUNNING, FAILURE };
     public Status status;
     public List<Node> children = new List<Node>();
     public int currentChild = 0;
-    public string name; 
+    public string name;
 
     public Node() { }
+
     public Node(string n)
     {
         name = n;
+    }
+
+    public virtual Status Process()
+    {
+        return children[currentChild].Process();
     }
 
     public void AddChild(Node n)
