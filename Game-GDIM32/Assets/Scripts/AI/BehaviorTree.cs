4523c8ed21fdd4e47cbea59ef6b89b8ce5828373
Hunter Drummond <85197375+HSDrummond@users.noreply.github.com>
2022-02-17 13:36:19 -0800

    Beginning of Behavior Tree

---
+++ Game-GDIM32/Assets/Scripts/AI/BehaviorTree.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorTree : Node
+{
+    public BehaviorTree()
+    {
+        name = "Tree";
+    }
+
+    public BehaviorTree(string n)
+    {
+        name = n;
+    }
+}
