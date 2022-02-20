e7673d9e89302ea5875c1e8b955598d46a2f87a9
cisincla <91297268+cisincla@users.noreply.github.com>
2022-02-20 18:34:53 -0500

    Update SpeedPowerUp.cs

--- Game-GDIM32/Assets/Scripts/Crops/SpeedPowerUp.cs
+++ Game-GDIM32/Assets/Scripts/Crops/SpeedPowerUp.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedPowerUp : monobehaviour {
-    public float increase = 5f;
+//public class SpeedPowerUp : monobehaviour {
+    //public float increase = 5f;
 
-    private void ontriggerenter2d(collider2d collision) {
-        if (collision.tag == "Player1") {
-            gameobject player = collision.gameobject;
-            Player1 playerscript = player.getcomponent<Player1>();
+    //private void OnTriggerEnter2D(Collider2D collision) {
+        //if (collision.tag == "Player1") {
+            //gameobject player = collision.gameobject;
+            //Player1 playerscript = player.getcomponent<Player1>();
 
-            if playerscript) {
-                playerscript.movespeed += increase;
+            //if playerscript) {
+                //playerscript.movespeed += increase;
             }
         }
     }
