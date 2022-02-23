3c665c3dc2178350231a114f5cfc88679873174c
cisincla <91297268+cisincla@users.noreply.github.com>
2022-02-23 14:37:54 -0500

    p2

---
+++ Game-GDIM32/Assets/PowerUpP2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpP2 : MonoBehaviour
+{
+
+    public float multiplier = 1.4f;
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player2"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        player.transform.localScale *= multiplier;
+
+        Destroy(gameObject);
+    }
+
+}
