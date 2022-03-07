a8e9872f0d319e954b754a97e05e69a3e228a038
dfu7 <78887201+dfu7@users.noreply.github.com>
2022-03-06 16:11:08 -0800

    separate icons work

--- Game-GDIM32/Assets/Scripts/Inventory/InventoryP2/Inventory2Slot.cs
+++ Game-GDIM32/Assets/Scripts/Inventory/InventoryP2/Inventory2Slot.cs
@@ -4,23 +4,16 @@ using UnityEngine.UI;
 
 public class Inventory2Slot : MonoBehaviour
 {
-
     public Image icon;
-
-    GameObject item;
 
     public void AddItem(GameObject newItem)
     {
-        item = newItem;
-
-        icon.sprite = item.GetComponent<SpriteRenderer>().sprite;
+        icon.sprite = newItem.GetComponent<PickUp>().icon;
         icon.enabled = true;
     }
 
     public void ClearSlot()
     {
-        item = null;
-
         icon.sprite = null;
         icon.enabled = false;
     }
